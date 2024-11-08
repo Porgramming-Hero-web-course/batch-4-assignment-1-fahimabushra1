480a942cb5a9e8d3db6fde451d19dcd91d8f7739
fatima arifeen <fatima.arifeen24@gmail.com>
2024-11-08 22:44:02 +0600

    final

--- B4A1/src/problem-8.ts
+++ B4A1/src/problem-8.ts
@@ -1,9 +1,8 @@
 // problem-8:
 
-const validateKeys = <T>(obj: T, keys: (keyof T)[]): boolean[] => {
-    return keys.map(key => key in obj);
+const validateKeys = <T>(obj: T, keys: (keyof T)[]): boolean => {
+    return keys.every(key => key in obj);
 }
 
-// Sample Input
 const person = { name: "Alice", age: 25, email: "alice@example.com" };
 console.log(validateKeys(person, ["name", "age"]));
